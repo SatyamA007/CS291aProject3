12cdcc373b1cade1ddfe7095babfc187b051a429
Satyam <satyamawasthi007@gmail.com>
2021-10-16 13:35:26 -0700

    Join callback and other minor fixes

--- server/app.rb
+++ server/app.rb
@@ -14,11 +14,14 @@ $streams = Hash.new() # stream token vs stream object
 $stokens = Hash.new() # username vs stream tokens
 $mtokens = Hash.new() # msg token vs stream tokens
 
+before do
+  headers 'Access-Control-Allow-Origin' => '*'
+end
 
 EventMachine.schedule do
   EventMachine.add_periodic_timer(SCHEDULE_TIME) do
     # Change this for any timed events you need to schedule.
-    puts "This message will be output to the server console every #{SCHEDULE_TIME} seconds"
+    #puts "This message will be output to the server console every #{SCHEDULE_TIME} seconds"
   end
 end
 
@@ -26,10 +29,31 @@ def timestamp
   Time.now.to_i
 end
 
-def sse_join(stream, username)
-  data = {created: "#{timestamp()}", user: "#{username}"}
-    stream << "event: join \n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+def sse_event(stream, event, username="", message="")
+  case event
+  when 'Join'
+    data = {user: username, created: timestamp()}
+    stream << "event: Join\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  when 'Disconnect'
+    data = {created: timestamp()}
+    stream << "event: Disconnect\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  when 'Message'
+    data = {message: message,user: username, created: timestamp()}
+    stream << "event: Message\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  when 'Part'
+    data = {user: username, created: timestamp()}
+    stream << "event: Part\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  when 'ServerStatus'
+    data = {status: username, created: timestamp()}
+    stream << "event: ServerStatus\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  when 'Users'
+    data = {users: $stokens.keys(), created: timestamp()}
+    stream << "event: Users\n" << "data: "<< data.to_json << "\n" << "id: " << SecureRandom.uuid << "\n\n"
+  else
+    stream << "event: error\n"<< SecureRandom.uuid << "\n\n"
+  end 
 end
+
 
 #----- /stream Endpoint -------
 
@@ -37,23 +61,24 @@ get '/stream/:token', provides: 'text/event-stream' do
   headers 'Access-Control-Allow-Origin' => '*'
 
   token = params['token']
-  username = $stokens.key(token)
   if($stokens.key(token) == nil)
     return [403, 'Invalid stream token']
   end
 
-  /strm = $streams.key(token)
-  if(strm == nil)
-    return [409, 'Connection already exists!']
-  end/
+  username = $stokens.key(token)
+  strm = $streams[token]
 
+  if(strm != nil)
+    return [409, 'Connection already exists!']
+  end
+  
   stream(:keep_open) do |connection|
     connections << connection
-
-    sse_join(connection, username)  #JOIN sse event
+    #$streams[token] = connection
+    sse_event(connection, "Join", username)  #Join sse event
 
     connection.callback do
-      puts 'callback'
+      sse_event(connection, "Disconnect")    #Disconnect sse event
       connections.delete(connection)
     end
   end
@@ -62,8 +87,6 @@ end
 #----- /login Endpoint -------
 
 post '/login' do
-  headers 'Access-Control-Allow-Origin' => '*'
-
   begin
     uname = params[:username]
     pwd = params[:password]
@@ -76,19 +99,18 @@ post '/login' do
       return [409, 'Multiple login tabs']
     end
 
-    if($users.has_key?("#{uname}"))
-      stored_pwd = $users.fetch("#{uname}")
+    if($users.has_key?(uname))
+      stored_pwd = $users[uname]
       if(stored_pwd != pwd)
         return [403, 'Invalid username/password']
       end
     else
-      $users["#{uname}"] = pwd
+      $users[uname] = pwd
     end
 
     strtoken = SecureRandom.hex(32)
     msgtoken = SecureRandom.hex
     $stokens[uname] = strtoken
-    #$mtokens[msgtoken] =
 
     body = {message_token: "#{msgtoken}", stream_token: "#{strtoken}"}
     headers 'Content-Type' => 'application/json'
@@ -102,32 +124,22 @@ end
 #----- /message Endpoint -------
 
 post '/message' do
-  require 'pp'
-
-  connections.each do |connection|
-    connection << "data: Goodbye!\n\n"
-    connection.close  # This call will trigger connection.callback
-  end
 
   puts 'Headers'
   PP.pp(request.env.filter { |x| x.start_with?('HTTP_') })
-  puts
 
   puts 'request.params:'
   PP.pp request.params
-  puts
 
   [403, "POST /message\n"]
 end
 
 #------- Options endpoint ---------
 
-options '/message', provides: 'text/event-stream' do
-  headers 'Access-Control-Allow-Origin' => '*'
-
+options '/message' do
   PP.pp("-- options --")
   PP.pp(params)
-  PP.pp("-----")
+  PP.pp(request.env)
   #PP.pp(request.env)
-
+  [404,"not found"]
 end
